d2dc160e5b80514baaac08c868fe189149569cf0
Jela Mae Gnilo <jmng2022-7904-97516@bicol-u.edu.ph>
2024-08-28 09:54:56 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Route for homepage
 Route::get('/homepage', function () {
     return view('homepage');
 });
 
+//Route for about
 Route::get('/about', function () {
     return view('about');
 });
 
+//Route for content
 Route::get('/content', function () {
     return view('content');
 });
